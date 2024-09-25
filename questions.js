e604fd3f3e3643581d066fac4ef149dcdda0586e
jpscavaco <142665518+jpscavaco@users.noreply.github.com>
2024-09-25 22:32:48 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,361 @@
+export const quizItems = [{
+  question: 'The human skeleton is made up of less than 100 bones',
+  answer: 'False'
+}, {
+  question: 'Mount Everest is the highest mountain in the world',
+  answer: 'True'
+}, {
+  question: 'The Great Wall of China can be seen from space',
+  answer: 'False'
+}, {
+  question: 'The Amazon is the longest river in the world',
+  answer: 'False'
+}, {
+  question: 'Sharks are mammals',
+  answer: 'False'
+}, {
+  question: 'The capital of Australia is Sydney',
+  answer: 'False'
+}, {
+  question: 'Lightning never strikes the same place twice',
+  answer: 'False'
+}, {
+  question: 'The human body is comprised of over 60% water',
+  answer: 'True'
+}, {
+  question: 'The currency of France is the Franc',
+  answer: 'False'
+}, {
+  question: 'Albert Einstein was awarded the Nobel Prize in Physics for the theory of relativity',
+  answer: 'False'
+}, {
+  question: 'Venus is the closest planet to the Sun',
+  answer: 'False'
+}, {
+  question: 'An octopus has three hearts',
+  answer: 'True'
+}, {
+  question: 'Humans have more than five senses',
+  answer: 'True'
+}, {
+  question: 'The Pyramids of Giza are located in Iraq',
+  answer: 'False'
+}, {
+  question: 'Sound travels faster in air than in water',
+  answer: 'False'
+}, {
+  question: 'The Internet and the World Wide Web are the same thing',
+  answer: 'False'
+}, {
+  question: 'The Mona Lisa was painted by Leonardo da Vinci',
+  answer: 'True'
+}, {
+  question: 'The human eye can distinguish about a million different colors',
+  answer: 'True'
+}, {
+  question: 'All planets in our solar system orbit the Sun in a circular path',
+  answer: 'False'
+}, {
+  question: 'The Titanic sank on its maiden voyage',
+  answer: 'True'
+}, {
+  question: 'A group of lions is known as a "pride"',
+  answer: 'True'
+}, {
+  question: 'Crocodiles have no sweat glands so they use their mouths to release heat',
+  answer: 'True'
+}, {
+  question: 'Camels store water in their humps',
+  answer: 'False'
+}, {
+  question: 'Ostriches bury their heads in the sand when they are scared',
+  answer: 'False'
+}, {
+  question: 'Butterflies taste with their feet',
+  answer: 'True'
+}, {
+  question: 'Cows have best friends',
+  answer: 'True'
+}, {
+  question: 'A group of crows is called a "murder"',
+  answer: 'True'
+}, {
+  question: 'Penguins are only found in Antarctica',
+  answer: 'False'
+}, {
+  question: 'Elephants are the only animals that can’t jump',
+  answer: 'False'
+}, {
+  question: 'Dolphins sleep with one eye open',
+  answer: 'True'
+}, {
+  question: 'A snail can sleep for three years',
+  answer: 'True'
+}, {
+  question: 'The fingerprints of a koala are virtually indistinguishable from those of a human',
+  answer: 'True'
+}, {
+  question: 'All spiders spin webs',
+  answer: 'False'
+}, {
+  question: 'The horn of a rhinoceros is made from bone',
+  answer: 'False'
+}, {
+  question: 'Flamingos are born pink',
+  answer: 'False'
+}, {
+  question: 'A male kangaroo is called a boomer',
+  answer: 'True'
+}, {
+  question: 'Honeybees have five eyes',
+  answer: 'True'
+}, {
+  question: 'Some octopuses can change their skin color and texture',
+  answer: 'True'
+}, {
+  question: 'Frogs can drink water through their skin',
+  answer: 'True'
+}, {
+  question: 'Turtles can come out of their shells',
+  answer: 'False'
+}, {
+  question: 'Water expands when it freezes',
+  answer: 'True'
+}, {
+  question: 'The Milky Way is a type of galaxy',
+  answer: 'True'
+}, {
+  question: 'Humans can survive without oxygen for up to an hour',
+  answer: 'False'
+}, {
+  question: 'DNA is the primary genetic material in humans and most other organisms',
+  answer: 'True'
+}, {
+  question: 'All radioactivity is man-made',
+  answer: 'False'
+}, {
+  question: 'Mercury is the hottest planet in the solar system',
+  answer: 'False'
+}, {
+  question: 'Atoms are most densely packed in gases',
+  answer: 'False'
+}, {
+  question: 'Photosynthesis occurs in animals as well as plants',
+  answer: 'False'
+}, {
+  question: 'The Earth’s atmosphere is mostly oxygen',
+  answer: 'False'
+}, {
+  question: 'The speed of light is constant in all mediums',
+  answer: 'False'
+}, {
+  question: 'Pluto is classified as a planet',
+  answer: 'False'
+}, {
+  question: 'Gold is a good conductor of electricity',
+  answer: 'True'
+}, {
+  question: 'The adult human body has 32 teeth',
+  answer: 'True'
+}, {
+  question: 'Antibiotics are effective against viruses',
+  answer: 'False'
+}, {
+  question: 'The Great Barrier Reef is visible from space',
+  answer: 'True'
+}, {
+  question: 'Mars is known as the “Red Planet” due to its blue skies',
+  answer: 'False'
+}, {
+  question: 'A kilogram of lead is heavier than a kilogram of feathers',
+  answer: 'False'
+}, {
+  question: 'Bats are blind',
+  answer: 'False'
+}, {
+  question: 'The smallest bone in the human body is located in the ear',
+  answer: 'True'
+}, {
+  question: 'Sound travels faster than light',
+  answer: 'False'
+}, {
+  question: 'The initial construction of the Great Wall of China began during the reign of Emperor Qin Shi Huang',
+  answer: 'True'
+}, {
+  question: 'The Titanic was discovered before the 20th century ended',
+  answer: 'True'
+}, {
+  question: 'The ancient Egyptian civilization was located along the Amazon River',
+  answer: 'False'
+}, {
+  question: 'World War I began in 1914',
+  answer: 'True'
+}, {
+  question: 'The United States Declaration of Independence was signed in 1776',
+  answer: 'True'
+}, {
+  question: 'Leonardo da Vinci painted the Sistine Chapel ceiling',
+  answer: 'False'
+}, {
+  question: 'The Roman Empire officially ended in the year 1000 AD',
+  answer: 'False'
+}, {
+  question: 'Queen Elizabeth I was the daughter of Henry VIII',
+  answer: 'True'
+}, {
+  question: 'The Aztec civilization was primarily located in what is now Canada',
+  answer: 'False'
+}, {
+  question: 'The first human landing on the Moon was in 1969',
+  answer: 'True'
+}, {
+  question: 'The Berlin Wall fell in the late 1980s',
+  answer: 'True'
+}, {
+  question: 'The Industrial Revolution began in the United States',
+  answer: 'False'
+}, {
+  question: 'Cleopatra was Egyptian by birth',
+  answer: 'False'
+}, {
+  question: 'The Hundred Years’ War lasted exactly one hundred years',
+  answer: 'False'
+}, {
+  question: 'Julius Caesar was the first Roman Emperor',
+  answer: 'False'
+}, {
+  question: 'The Magna Carta was signed in 1215',
+  answer: 'True'
+}, {
+  question: 'The Renaissance began in the 14th century',
+  answer: 'True'
+}, {
+  question: 'Mahatma Gandhi was the first Prime Minister of India',
+  answer: 'False'
+}, {
+  question: 'The Spanish Armada was defeated by the English in 1588',
+  answer: 'True'
+}, {
+  question: 'The Black Death occurred in the 14th century',
+  answer: 'True'
+}, {
+  question: '“Parasite” (2019) was the first non-English language film to win the Academy Award for Best Picture',
+  answer: 'True'
+}, {
+  question: '“Oppenheimer” (2023) won 7 Oscars, including Best Picture, in the 96th Academy Awards',
+  answer: 'True'
+}, {
+  question: '“Joker” (2019) won the Academy Award for Best Cinematography',
+  answer: 'False'
+}, {
+  question: '“The Irishman” (2019) is a film directed by Quentin Tarantino',
+  answer: 'False'
+}, {
+  question: '“1917” (2019) is a World War II film',
+  answer: 'False'
+}, {
+  question: '“Tenet” (2020) was directed by Christopher Nolan',
+  answer: 'True'
+}, {
+  question: '“Soul” (2020) is a Pixar animation that explores the afterlife',
+  answer: 'True'
+}, {
+  question: 'Emma Stone won Best Actress for her role as Bella Baxter in the movie “Poor Things”',
+  answer: 'True'
+}, {
+  question: '“The Lion King” (2019) was entirely shot in live-action',
+  answer: 'False'
+}, {
+  question: '“Knives Out” (2019) is a romantic comedy',
+  answer: 'False'
+}, {
+  question: '“Jojo Rabbit” (2019) was directed by Taika Waititi',
+  answer: 'True'
+}, {
+  question: '“Little Women” (2019) is based on a novel from the 21st century',
+  answer: 'False'
+}, {
+  question: '“Once Upon a Time in Hollywood” (2019) features Leonardo DiCaprio and Brad Pitt',
+  answer: 'True'
+}, {
+  question: '“Dune” (2021) is a standalone film with no planned sequels',
+  answer: 'False'
+}, {
+  question: '“The Trial of the Chicago 7” (2020) is based on true events',
+  answer: 'True'
+}, {
+  question: '“Mulan” (2020) includes the character Mushu from the original animated film',
+  answer: 'False'
+}, {
+  question: '“A Star is Born” (2018) marks Lady Gaga’s film debut',
+  answer: 'False'
+}, {
+  question: '“Frozen II” (2019) became the highest-grossing animated film ever at the time of its release',
+  answer: 'True'
+}, {
+  question: '“Black Widow” (2021) is the first Marvel film centered around a female superhero',
+  answer: 'False'
+}, {
+  question: '“Ford v Ferrari” (2019) is based on a true story',
+  answer: 'True'
+}, {
+  question: '“Its” and “It’s” have the same meaning',
+  answer: 'False'
+}, {
+  question: '“Whom” is used as an object, while “Who” is used as a subject',
+  answer: 'True'
+}, {
+  question: 'An adverb can modify a noun',
+  answer: 'False'
+}, {
+  question: '“You and I” is always correct, and “You and me” is always incorrect',
+  answer: 'False'
+}, {
+  question: 'The Oxford comma is a mandatory element in English grammar',
+  answer: 'False'
+}, {
+  question: '“Could of” is a correct phrase in English grammar',
+  answer: 'False'
+}, {
+  question: 'A split infinitive is grammatically incorrect',
+  answer: 'False'
+}, {
+  question: 'Passive voice should always be avoided in writing',
+  answer: 'False'
+}, {
+  question: '“Affect” is a verb, and “effect” is a noun',
+  answer: 'True'
+}, {
+  question: 'A preposition is always found at the beginning of a prepositional phrase',
+  answer: 'True'
+}, {
+  question: '“Their,” “There,” and “They’re” are interchangeable',
+  answer: 'False'
+}, {
+  question: 'Every sentence must contain a verb',
+  answer: 'True'
+}, {
+  question: '“Fewer” is used for countable items, and “Less” is used for uncountable quantities',
+  answer: 'True'
+}, {
+  question: '“Whose” and “Who’s” have the same usage',
+  answer: 'False'
+}, {
+  question: '“I.e.” and “e.g.” can be used interchangeably',
+  answer: 'False'
+}, {
+  question: 'All sentences must have a subject',
+  answer: 'True'
+}, {
+  question: 'Double negatives in a sentence create a positive meaning',
+  answer: 'True'
+}, {
+  question: '“Lie” and “Lay” mean the same thing and can be used interchangeably',
+  answer: 'False'
+}, {
+  question: 'An adjective always precedes the noun it modifies',
+  answer: 'False'
+}, {
+  question: '“Your” and “You’re” mean the same thing',
+  answer: 'False'
+}]
---
+++ tic-tac-know.js
@@ -0,0 +1,251 @@
+import { quizItems } from "./questions.js";
+
+let board = ['', '', '', '', '', '', '', '', ''];
+let askedQuizItems = [];
+
+displayStartGameButton(); 
+
+function displayStartGameButton() {
+  const gameContainer = document.querySelector('.js-game-container');
+  const gameContainerOriginalHTML = document.querySelector('.js-game-container').innerHTML;  
+  
+  gameContainer.innerHTML = ''; 
+
+  let timeOutId; 
+  
+  timeOutId = setInterval(()=> {
+    if (gameContainer.innerHTML === '') {
+      gameContainer.innerHTML = `
+        <button class="start-game-button js-start-game-button">
+          CLICK TO PLAY
+        </button>`;
+
+      document.querySelector('.js-start-game-button')
+        .addEventListener('click', ()=> {
+          gameContainer.innerHTML = gameContainerOriginalHTML;
+          clearInterval(timeOutId);   
+          gameIntro();
+        });
+      
+    } else {
+      gameContainer.innerHTML = '';      
+    }
+  }, 1000); 
+}
+
+function gameIntro() {  
+  const gameText = document.querySelector('.js-game-text');
+
+  setTimeout(() => {
+    gameText.innerHTML = `
+      <p>Welcome to <span class="bold">TIC · TAC · KNOW</span></p>
+    `;    
+  }, 1000);   
+
+  setTimeout(() => {
+    gameText.innerHTML = `
+      <p>The game where you test your <span class="bold">Tic Tac Toe</span> and</p>
+      <p><span class="bold">General Knowledge</span> skills at the same time</p>
+    `;
+  }, 4000);   
+
+  setTimeout(() => {
+    gameText.innerHTML = `<p>Good Luck!</p>`;    
+  }, 10000); 
+
+  setTimeout(() => {    
+    playGame(gameText);
+  }, 14000); 
+}
+
+function playGame(gameText) {  
+  let turn = 1;
+  let player = 'Player 1';
+  let play = 'O';  
+  let gameOver = false; 
+  let timeOutId;
+
+  activateSpotsButtons();
+  askForMove(gameText, player);  
+
+  // Sets the event listeners for the buttons as the nine spots in the board
+  document.querySelectorAll('.js-board-button')
+    .forEach((spot, index) => {  
+      spot.addEventListener('click', () => {        
+        clearTimeout(timeOutId);        
+        
+        // Marks a temporary play and asks a random question if the spot is available in the board
+        if (!board[index]) {
+          deactivateSpotsButtons();
+          updateBoard(play, index);
+          renderBoard();
+
+          const randomQuizItem = getRandomQuizItem();       
+          const question = randomQuizItem.question; 
+          const answer = randomQuizItem.answer;
+
+          gameText.innerHTML = `            
+            <div class="question">${question}</div>
+            <button class="answer-button js-answer-button">True</button>
+            <button class="answer-button js-answer-button">False</button>                  
+          `;
+
+          // Sets the event listeners for the answer buttons as 'True' and 'False'
+          document.querySelectorAll('.js-answer-button')
+            .forEach((button) => {
+              button.addEventListener('click', ()=> {            
+                const playerAnswer = getPlayerAnswer(button, answer); 
+
+                // Confirms the move in the board if the answer is correct
+                if (playerAnswer) { 
+                  gameText.innerHTML = 'You are right!';                
+                  spot.classList.add('board-button-confirmed-play');               
+
+                  // Checks if the the game is over after each play                                    
+                  gameOver = checkGameOver(gameText, player);  
+                  
+                  if (gameOver) {           
+                    deactivateSpotsButtons();
+                    
+                    setTimeout(() => {
+                      gameText.innerHTML = `<p>GAME OVER</p>`;
+                    }, 4000); 
+        
+                    setTimeout(() => {
+                      restartGame();  
+                    }, 8000); 
+                  
+                    return;
+                  }                   
+                  
+                // The current player loses the turn if the answer is incorrect 
+                } else {
+                  gameText.innerHTML = 'Wrong answer!';  
+                  updateBoard('', index);
+                  renderBoard();
+                }                                
+                
+                // Changes the player's turn to play
+                turn *= -1;
+                player = getPlayer(turn);
+                play = getPlay(turn);
+                
+                setTimeout(() => {                  
+                  askForMove(gameText, player); 
+                  activateSpotsButtons();              
+                }, 3000);                
+              });        
+            });       
+        
+        // Play again if the spot is already taken
+        } else {
+          gameText.innerHTML = `<p>That spot is already taken!</p>`;
+
+          timeOutId = setTimeout(() => {
+            askForMove(gameText, player);            
+          }, 3000);
+        }       
+      });
+    });  
+}
+
+function askForMove(gameText, player) {
+  gameText.innerHTML = `<p>It's your turn, <span class="bold">${player}</span></p>`;  
+}
+
+function getPlayer(turn) {  
+  return turn === 1? 'Player 1' : 'Player 2';
+}
+
+function getPlay(turn) {
+  return turn === 1? 'O' : 'X';
+}
+
+function getRandomQuizItem() {
+  let randomQuizItem;
+  
+  while (true) {
+    randomQuizItem = quizItems[(Math.floor(Math.random() * quizItems.length))];    
+
+    // Checks if the question has already been asked
+    if (!askedQuizItems.includes(randomQuizItem)) {      
+      askedQuizItems.push(randomQuizItem);      
+
+      // The asked questions list is reset if all the questons have been asked
+      askedQuizItems = askedQuizItems.length === quizItems.length ? [randomQuizItem] : askedQuizItems;      
+      
+      return randomQuizItem;
+    }      
+  }   
+}
+
+function getPlayerAnswer(button, answer) {  
+  return button.innerText === answer ? true : false;       
+}
+
+function updateBoard(play, index) {
+  board[index] = play;
+}
+
+function renderBoard() {  
+  document.querySelectorAll('.js-board-button')
+    .forEach((spot, index) => {
+      spot.innerHTML = board[index];  
+    }); 
+}
+
+function activateSpotsButtons() {
+  document.querySelectorAll('.js-board-button')
+    .forEach((spot) => {
+      spot.disabled = false;
+      spot.classList.add('board-button-ON');
+      spot.classList.add('board-button-ON:hover');
+      spot.classList.add('board-button-ON:active');
+    });
+}
+
+function deactivateSpotsButtons() {
+  document.querySelectorAll('.js-board-button')
+    .forEach((spot) => {
+      spot.disabled = true; 
+      spot.classList.remove('board-button-ON');
+      spot.classList.remove('board-button-ON:hover');
+      spot.classList.remove('board-button-ON:active');
+    });
+}
+
+function removeConfirmedPlayCSS() {
+  document.querySelectorAll('.js-board-button')
+    .forEach((spot, index) => {
+      spot.classList.remove('board-button-confirmed-play');  
+    });   
+};  
+
+function checkGameOver(gameText, player) {  
+  //CHECKS WIN   
+  if (
+    // CHECKS HORIZONTAL LINES
+    (board[0] === board[1] && board[0] === board[2] && board[0] != '') || (board[3] === board[4] && board[3] === board[5] && board[3] != '') || (board[6] === board[7] && board[6] === board[8] && board[6] != '') ||
+    // CHECKS VERTICAL LINES
+    (board[0] === board[3] && board[0] === board[6] && board[0] != '') || (board[1] === board[4] && board[1] === board[7] && board[1] != '') || (board[2] === board[5] && board[2] === board[8] && board[2] != '') ||
+    // CHECKS DIAGONAL LINES
+    (board[0] === board[4] && board[0] === board[8] && board[0] != '') || (board[2] === board[4] && board[2] === board[6] && board[2] != '')
+  ) {    
+    gameText.innerHTML = `<p>${player} Wins!</p>`;    
+
+    return true;
+  
+    //CHECKS TIE 
+  } else if (!board.includes('')) {
+    gameText.innerHTML = `<p>It's a draw!</p>`;   
+    
+    return true;  
+  }
+}
+
+function restartGame() {  
+  board = ['', '', '', '', '', '', '', '', ''];   
+  renderBoard();  
+  removeConfirmedPlayCSS();  
+  displayStartGameButton();
+}
